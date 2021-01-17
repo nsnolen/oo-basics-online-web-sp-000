e1bdc379549d94da956708f8d4e8b2800352dbbf
Sara Nolen <nolensara1@gmail.com>
2021-01-17 16:49:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,7 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  
+
   def initialize(brand)
     @brand = brand
   end
@@ -6,4 +9,8 @@ class Shoe
   def brand
     @brand
   end
+
+  def cobbler(condition)
+    puts "The shoe has been repaired!"
+    shoe.condition = new
 end
